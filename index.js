7f970423fe87357ecc49e83009e45d6a37c3d9a0
Mufaddal <mufaddaltopiwala15@gmail.com>
2024-06-22 01:28:46 +0530

    formatting

--- index.js
+++ index.js
@@ -1,48 +1,41 @@
 let homeEl = document.getElementById("home-score")
 let guestEl = document.getElementById("guest-score")
-let homeScore=0
-let guestScore=0
+let homeScore = 0
+let guestScore = 0
 
-function homeInc1()
-{
-    homeScore+=1
-    homeEl.textContent= homeScore
+function homeInc1() {
+    homeScore += 1
+    homeEl.textContent = homeScore
 }
 
-function homeInc2()
-{
-    homeScore+=2
-    homeEl.textContent= homeScore
+function homeInc2() {
+    homeScore += 2
+    homeEl.textContent = homeScore
 }
 
-function homeInc3()
-{
-    homeScore+=3
-    homeEl.textContent= homeScore
+function homeInc3() {
+    homeScore += 3
+    homeEl.textContent = homeScore
 }
 
-function guestInc1()
-{
-    guestScore+=1
-    guestEl.textContent= guestScore
+function guestInc1() {
+    guestScore += 1
+    guestEl.textContent = guestScore
 }
 
-function guestInc2()
-{
-    guestScore+=2
-    guestEl.textContent= guestScore
+function guestInc2() {
+    guestScore += 2
+    guestEl.textContent = guestScore
 }
 
-function guestInc3()
-{
-    guestScore+=3
-    guestEl.textContent= guestScore
+function guestInc3() {
+    guestScore += 3
+    guestEl.textContent = guestScore
 }
 
-function reset()
-{
-    homeScore=0
-    guestScore=0
-    guestEl.textContent= 0
-    homeEl.textContent= 0
+function reset() {
+    homeScore = 0
+    guestScore = 0
+    guestEl.textContent = 0
+    homeEl.textContent = 0
 }

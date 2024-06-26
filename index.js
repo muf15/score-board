c5e692bc12c72e563ba1e4be1613cacb885f430b
Mufaddal <mufaddaltopiwala15@gmail.com>
2024-06-26 15:45:35 +0530

    js changes

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
-let homeEl = document.getElementById("home-score")
-let guestEl = document.getElementById("guest-score")
+const homeEl = document.getElementById("home-score")
+const guestEl = document.getElementById("guest-score")
+const homeInc1 = document.getElementById("home-inc-1")
+const homeInc2 = document.getElementById("home-inc-2")
+const homeInc3 = document.getElementById("home-inc-3")
+const guestInc1 = document.getElementById("guest-inc-1")
+const guestInc2 = document.getElementById("guest-inc-2")
+const guestInc3 = document.getElementById("guest-inc-3")
+const reset = document.getElementById("reset")
 let homeScore = 0
 let guestScore = 0
 
-function homeInc1() {
+
+homeInc1.addEventListener("click",function(){
     homeScore += 1
     homeEl.textContent = homeScore
-}
+})
 
-function homeInc2() {
+homeInc2.addEventListener("click",function(){
     homeScore += 2
     homeEl.textContent = homeScore
-}
+})
 
-function homeInc3() {
+homeInc3.addEventListener("click",function(){
     homeScore += 3
     homeEl.textContent = homeScore
-}
+})
 
-function guestInc1() {
+guestInc1.addEventListener("click",function(){
     guestScore += 1
     guestEl.textContent = guestScore
-}
+})
 
-function guestInc2() {
+guestInc2.addEventListener("click",function(){
     guestScore += 2
     guestEl.textContent = guestScore
-}
+})
 
-function guestInc3() {
+guestInc3.addEventListener("click",function(){
     guestScore += 3
     guestEl.textContent = guestScore
-}
+})
 
-function reset() {
+reset.addEventListener("click",function() {
     homeScore = 0
     guestScore = 0
     guestEl.textContent = 0
     homeEl.textContent = 0
-}
+})
